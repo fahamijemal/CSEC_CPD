a6934a9104e0e5499e4ac8084ea43c66e33f06ba
Fahami Jemal <132432021+fahamijemal@users.noreply.github.com>
2025-02-06 16:39:35 +0300

    The solution for the  problem  "Word.py"

---
+++ Word.py
@@ -0,0 +1,8 @@
+s = input().strip()
+ 
+uppercase_count = sum(1 for c in s if c.isupper())
+lowercase_count = len(s) - uppercase_count 
+ 
+if uppercase_count > lowercase_count:
+    print(s.upper())
+else:
