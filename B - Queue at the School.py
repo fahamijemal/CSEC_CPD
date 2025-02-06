57687a5f9b9096faa907f5ab62ba953c0ad25577
Fahami Jemal <132432021+fahamijemal@users.noreply.github.com>
2025-02-06 18:20:00 +0300

    The solution for "B - Queue at the School.py"

---
+++ B - Queue at the School.py
@@ -0,0 +1,15 @@
+n, t = map(int, input().split())
+queue = list(input().strip())
+ 
+
+for _ in range(t):
+    i = 0
+    while i < len(queue) - 1:
+        if queue[i] == 'B' and queue[i + 1] == 'G':
+          
+            queue[i], queue[i + 1] = queue[i + 1], queue[i]
+            i += 1  
+        i += 1
+ 
+
+print("".join(queue))
